b1b742c63e3ea2ca4a0991c749879301a5093e30
Luke Gibbins <bg85lk@student.sunderland.ac.uk>
2018-11-05 22:30:37 +0000

    login working

--- app/src/main/java/com/example/quoteme/SignUpActivity.java
+++ app/src/main/java/com/example/quoteme/SignUpActivity.java
@@ -1,13 +1,29 @@
 package com.example.quoteme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        buttonSubmit = findViewById(R.id.buttonSignUp);
+        buttonSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonSubmit){
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+        }
     }
 }
