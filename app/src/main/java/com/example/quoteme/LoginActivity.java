677e442e70aeaf79c0f77e4783af014435fee09a
Luke Gibbins <bg85lk@student.sunderland.ac.uk>
2018-11-05 22:14:51 +0000

    Sign up now navigating

--- app/src/main/java/com/example/quoteme/LoginActivity.java
+++ app/src/main/java/com/example/quoteme/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.example.quoteme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView textSignUpNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textSignUpNow = findViewById(R.id.textSignUpNow);
+        textSignUpNow.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == textSignUpNow) {
+            Intent i = new Intent(this, SignUpActivity.class);
+            startActivity(i);
+        }
     }
 }
