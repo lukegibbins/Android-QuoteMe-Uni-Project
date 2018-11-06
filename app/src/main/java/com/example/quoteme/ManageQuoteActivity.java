48b4ba5b5c682e4b56a632ddcd84871c9771e7ba
Luke Gibbins <bg85lk@student.sunderland.ac.uk>
2018-11-06 14:20:46 +0000

    Finished. Now to decorate

--- app/src/main/java/com/example/quoteme/ManageQuoteActivity.java
+++ app/src/main/java/com/example/quoteme/ManageQuoteActivity.java
@@ -1,12 +1,7 @@
 package com.example.quoteme;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class ManageQuoteActivity extends AppCompatActivity {
 
@@ -14,24 +9,5 @@ public class ManageQuoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_quote);
-
-        final Intent i = new Intent(this, RequestQuoteActivity.class);
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Click 'Action to request a new quote", Snackbar.LENGTH_LONG)
-                        .setAction("Action", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                startActivity(i);
-                            }
-                        }).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
 }
