119b93b1ccb52e6a95f1c4e966adc0c66993957d
Luke Gibbins <bg85lk@student.sunderland.ac.uk>
2018-11-06 10:40:12 +0000

    Added image

--- app/src/main/java/com/example/quoteme/RequestQuoteActivity.java
+++ app/src/main/java/com/example/quoteme/RequestQuoteActivity.java
@@ -1,13 +1,32 @@
 package com.example.quoteme;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class RequestQuoteActivity extends AppCompatActivity {
+import es.dmoral.toasty.Toasty;
+
+public class RequestQuoteActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_quote);
+
+
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+        buttonSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonSubmit) {
+            Toasty.info(this, getString(R.string.app_success), Toast.LENGTH_SHORT).show();
+        }
     }
 }
