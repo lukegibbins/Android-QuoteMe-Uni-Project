22067ef14f87cbd63515018aee8765c5c922290e
Luke Gibbins <bg85lk@student.sunderland.ac.uk>
2018-11-06 11:33:33 +0000

    Added manage Quote

--- app/src/main/java/com/example/quoteme/RequestQuoteActivity.java
+++ app/src/main/java/com/example/quoteme/RequestQuoteActivity.java
@@ -3,6 +3,7 @@ package com.example.quoteme;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -28,5 +29,11 @@ public class RequestQuoteActivity extends AppCompatActivity implements View.OnCl
         if (v == buttonSubmit) {
             Toasty.info(this, getString(R.string.app_success), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
