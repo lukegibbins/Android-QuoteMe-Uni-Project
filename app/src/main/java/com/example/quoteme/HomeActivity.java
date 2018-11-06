553223926bc17ec799424bed569d872bb683311b
Luke Gibbins <bg85lk@student.sunderland.ac.uk>
2018-11-06 09:10:28 +0000

    Added class

--- app/src/main/java/com/example/quoteme/HomeActivity.java
+++ app/src/main/java/com/example/quoteme/HomeActivity.java
@@ -5,43 +5,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button requestWork;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        requestWork = findViewById(R.id.buttonRequestWork);
+        requestWork.setOnClickListener(this);
+
     }
 
-    //OnCreate, add the menu_main.xml (menu with sub-menus) to the options bar
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v == requestWork){
+            Intent i  = new Intent(this, RequestQuoteActivity.class);
+            startActivity(i);
+        }
+    }
 
-    //On menu and submenu selection
+
 //    @Override
 //    public boolean onOptionsItemSelected(MenuItem item) {
 //        switch(item.getItemId()){
 //            case R.id.action_background_red:
 //                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorRed));
 //                return true;
-//
-//            case R.id.action_background_blue:
-//                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorBlue));
-//                return true;
-//
-//            case R.id.action_background_default:
-//                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorDefault));
-//                return true;
-//
-//            case R.id.action_user:
-//                Intent intent = new Intent(this, Profile.class);
-//                startActivity(intent);
-//
 //            default:
 //                return super.onOptionsItemSelected(item);
 //        }
---
+++ app/src/main/java/com/example/quoteme/RequestQuoteActivity.java
@@ -0,0 +1,13 @@
+package com.example.quoteme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestQuoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_quote);
+    }
+}
