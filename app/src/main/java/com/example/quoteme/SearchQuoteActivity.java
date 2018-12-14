4f6de4a1b76c68800c755de350712d6b45d98e3d
Luke Gibbins <bg85lk@student.sunderland.ac.uk>
2018-12-14 15:44:03 +0000

    progress

--- app/src/main/java/com/example/quoteme/SearchQuoteActivity.java
+++ app/src/main/java/com/example/quoteme/SearchQuoteActivity.java
@@ -12,22 +12,27 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.quoteme.QuoteData.QuoteContract;
 import com.example.quoteme.QuoteData.QuoteCursorAdapter;
+import com.example.quoteme.UserData.UserContract;
 
-public class SearchQuoteActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+public class SearchQuoteActivity extends AppCompatActivity implements View.OnClickListener {
 
     private QuoteCursorAdapter quoteCursorAdapter;
-    private static final int QUOTE_LOADER = 0;
+    EditText filteredLocation;
+    Button buttonFilterSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_quote);
+
+        filteredLocation = findViewById(R.id.editLocationSearch);
 
         //Defines view which holds data queried by cursorAdapter
         ListView quoteListView = findViewById(R.id.search_list);
@@ -36,17 +41,34 @@ public class SearchQuoteActivity extends AppCompatActivity implements LoaderMana
         View emptyView = findViewById(R.id.search_empty_title_text);
         quoteListView.setEmptyView(emptyView);
 
-        //Create instance of cursorAdapter and bind cursorAdapter data to quoteList
-        quoteCursorAdapter = new QuoteCursorAdapter(this,null);
-        quoteListView.setAdapter(quoteCursorAdapter);
+        //Defines the items which should be displayed in quoteListView
+        String [] project = {
+                QuoteContract.QuoteEntry._ID,
+                QuoteContract.QuoteEntry.COLUMN_QUOTE_TITLE,
+                QuoteContract.QuoteEntry.COLUMN_QUOTE_VENDOR
+        };
 
-        getSupportLoaderManager().initLoader(QUOTE_LOADER, null, this);
+        Cursor cursor = getContentResolver().query(QuoteContract.QuoteEntry.CONTENT_URI,
+                project,
+                null,
+                null,
+                null
+        );
+
+        //Create instance of cursorAdapter and bind cursorAdapter data to quoteList
+        quoteCursorAdapter = new QuoteCursorAdapter(this, cursor);
+        quoteListView.setAdapter(quoteCursorAdapter);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    private void filterSearch(){
+
+
     }
 
     @Override
@@ -65,37 +87,9 @@ public class SearchQuoteActivity extends AppCompatActivity implements LoaderMana
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-    }
+    public void onClick(View v) {
+        if(v == buttonFilterSearch){
 
-    @NonNull
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
-
-        //Defines the items which should be displayed in quoteListView
-        String [] projection = {
-                QuoteContract.QuoteEntry._ID,
-                QuoteContract.QuoteEntry.COLUMN_QUOTE_TITLE,
-                QuoteContract.QuoteEntry.COLUMN_QUOTE_VENDOR
-        };
-
-        return new CursorLoader(this,
-                QuoteContract.QuoteEntry.CONTENT_URI,
-                projection,
-                null,
-                null,
-                null
-        );
-    }
-
-    @Override
-    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
-        quoteCursorAdapter.swapCursor(cursor);
-    }
-
-    @Override
-    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
-        quoteCursorAdapter.swapCursor(null);
+        }
     }
 }
