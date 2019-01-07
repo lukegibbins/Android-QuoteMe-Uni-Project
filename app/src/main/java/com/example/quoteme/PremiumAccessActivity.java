de5ea3a61ac80c7d42cffab3e5dfb63cb2b1b0ca
Luke Gibbins <bg85lk@student.sunderland.ac.uk>
2019-01-07 18:41:27 +0000

    amended design

--- app/src/main/java/com/example/quoteme/PremiumAccessActivity.java
+++ app/src/main/java/com/example/quoteme/PremiumAccessActivity.java
@@ -1,6 +1,12 @@
 package com.example.quoteme;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +23,19 @@ public class PremiumAccessActivity extends AppCompatActivity implements View.OnC
 
         buttonViewMap = findViewById(R.id.buttonMaps);
         buttonViewMap.setOnClickListener(this);
+
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+
+        System.out.println(longitude + "***LONG****");
+        System.out.println(latitude + "****LAT***");
+
     }
 
     @Override
