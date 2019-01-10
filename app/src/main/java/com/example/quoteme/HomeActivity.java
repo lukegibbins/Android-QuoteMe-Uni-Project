7a2db1defba7b5bfe4d572a1ab340e0409356c8f
Luke Gibbins <bg85lk@student.sunderland.ac.uk>
2019-01-10 17:42:12 +0000

    fgf

--- app/src/main/java/com/example/quoteme/HomeActivity.java
+++ app/src/main/java/com/example/quoteme/HomeActivity.java
@@ -71,7 +71,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main_notif, menu);
         return true;
     }
 
